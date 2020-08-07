14bccf479288cc078e4f83f22a2e47910587bdc1
dainiuxt <dainiuxt@gmail.com>
2020-08-07 10:49:43 +0300

    add ajax functionality for search and handle errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,14 +4,20 @@ class StocksController < ApplicationController
 		if params[:stock].present?
 			@stock = Stock.new_lookup(params[:stock])
 			if @stock
-				render 'users/my_portfolio'
+				respond_to do |format|
+					format.js { render partial: 'users/result' }
+				end
 			else
-				flash[:alert] = "Symbol not found, please try again"
-				redirect_to my_portfolio_path
+				respond_to do |format|
+					flash.now[:alert] = "Ticker not found, please try again"
+					format.js { render partial: 'users/result' }
+				end
 			end
 		else
-			flash[:alert] = "Please enter a symbol to search"
-			redirect_to my_portfolio_path
+			respond_to do |format|
+				flash.now[:alert] = "Please enter a ticker to search"
+				format.js { render partial: 'users/result' }
+			end
 		end
 	end
 
