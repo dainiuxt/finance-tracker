88ccdc6f66f37d621b523d430f9ace64f49982bf
dainiuxt <dainiuxt@gmail.com>
2020-08-13 13:47:27 +0300

    deploy production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   def my_portfolio
+    @user = current_user
   	@tracked_stocks = current_user.stocks
   end
 
@@ -8,25 +9,31 @@ class UsersController < ApplicationController
   	@friends = current_user.friends
   end
 
-	def search
-		if params[:friend].present?
-			@friend = params[:friend]
-			if @friend
-				respond_to do |format|
-					format.js { render partial: 'users/friend_result' }
-				end
-			else
-				respond_to do |format|
-					flash.now[:alert] = "Couldn't find user with such name or email"
-					format.js { render partial: 'users/friend_result' }
-				end
-			end
-		else
-			respond_to do |format|
-				flash.now[:alert] = "Please enter friend name or email to search"
-				format.js { render partial: 'users/friend_result' }
-			end
-		end
-	end
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end    
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
 
 end
